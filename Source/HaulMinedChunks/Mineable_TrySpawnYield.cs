ec7b7c6f2273095e768c2b58b20f4804f579343d
Mlie <emipa606@users.noreply.github.com>
2022-04-30 20:38:42 +0200

    Should now only mark chunks mined by colonists

--- Source/HaulMinedChunks/Mineable_TrySpawnYield.cs
+++ Source/HaulMinedChunks/Mineable_TrySpawnYield.cs
@@ -7,8 +7,13 @@ namespace HaulMinedChunks;
 [HarmonyPatch(typeof(Mineable), "TrySpawnYield")]
 internal class Mineable_TrySpawnYield
 {
-    private static void Postfix(Mineable __instance, Map map)
+    private static void Postfix(Mineable __instance, Map map, Pawn pawn)
     {
+        if (pawn?.IsColonist == false)
+        {
+            return;
+        }
+
         var possibleChunk = __instance.Position.GetFirstHaulable(map);
         if (possibleChunk == null ||
             possibleChunk.def.thingCategories?.Contains(ThingCategoryDefOf.StoneChunks) == false)
