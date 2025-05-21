f1a1d0982c4f0664ffa72ecc6deb2fda195d4253
Mlie <emipa606@users.noreply.github.com>
2025-05-21 22:18:19 +0200

    Added a minor failsafe for chunk spawning

--- Source/HaulMinedChunks/GenPlace_SplitAndSpawnOneStackOnCell.cs
+++ Source/HaulMinedChunks/GenPlace_SplitAndSpawnOneStackOnCell.cs
@@ -8,8 +8,13 @@ namespace HaulMinedChunks;
 [HarmonyPatch(typeof(GenPlace), "SplitAndSpawnOneStackOnCell")]
 public static class GenPlace_SplitAndSpawnOneStackOnCell
 {
-    public static void Postfix(Thing resultingThing)
+    public static void Postfix(Thing resultingThing, bool __result)
     {
+        if (!__result)
+        {
+            return;
+        }
+
         if (!Mineable_TrySpawnYield.Spawning)
         {
             return;
